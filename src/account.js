d146a2134da8bed750d5523e1045a6a480f5dc30
Jonny Abrams <jonnyabrams@hotmail.com>
2022-04-25 10:42:08 +0100

    Changed names of private functions

--- src/account.js
+++ src/account.js
@@ -14,21 +14,21 @@ class Account {
 
   deposit(amount, date = new Date().toLocaleDateString('en-GB')) {
     this.balance += amount;
-    this._recordDeposit(amount, date);
+    this.#recordDeposit(amount, date);
   }
 
   withdraw(amount, date = new Date().toLocaleDateString('en-GB')) {
     if(amount > this.balance) throw new Error('Insufficient funds');
   
     this.balance -= amount;
-    this._recordWithdrawal(amount, date);
+    this.#recordWithdrawal(amount, date);
   }
 
-  _recordDeposit(amount, date) {
+  #recordDeposit(amount, date) {
     this.transactions.push(`${date} || ${amount} || || £${this.getBalance()}`);
   }
 
-  _recordWithdrawal(amount, date) {
+  #recordWithdrawal(amount, date) {
     this.transactions.push(`${date} || || ${amount} || £${this.getBalance()}`);
   }
 }
