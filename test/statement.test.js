16e357240033e9b423a946a8f62f8fc0af4a7cc0
Jonny Abrams <jonnyabrams@hotmail.com>
2022-04-27 17:41:52 +0100

    Passed first 2 unit tests for Statement class

--- test/statement.test.js
+++ test/statement.test.js
@@ -1,1 +1,15 @@
 const Statement = require('../src/statement');
+
+beforeEach(() => {
+  statement = new Statement;
+});
+
+describe('Statement', () => {
+  it('is an instance of the Statement class', () => {
+    expect(statement).toBeInstanceOf(Statement);
+  });
+
+  it('has an empty array for storing transactions', () => {
+    expect(statement.transactions).toEqual([])
+  });
+});
